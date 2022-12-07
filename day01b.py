42fb280874f7417f248150d214dc4aa639e2ca7b
Nathan Kitchen <nathan.kitchen@gmail.com>
2022-12-06 19:41:09 -0800

    Day 1

---
+++ day01b.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import sys
+
+DEBUG = os.environ.get("DEBUG")
+
+def main():
+    inp = open(sys.argv[1]).read()
+    inventory_per_elf = inp.split('\n\n')
+    calories = [np.fromstring(inv, dtype=int, sep='\n')
+                for inv in inventory_per_elf]
+
+    n = max(len(c) for c in calories)
+    for i, _ in enumerate(calories):
+        calories[i].resize(n, refcheck=False)
+
+    calories = np.array(calories)
+
+    carried = calories.sum(axis=1)
+    carried.sort()
+    t = carried[-3:].sum()
+    print(t)
+
+main()
